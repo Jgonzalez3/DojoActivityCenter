af8072fc9529ea4be6a6b69f0b8d82bb1d7be429
Javier Gonzalez <javig56@gmail.com>
2018-06-15 22:00:34 -0700

    updated Validation View Model required error messages for non-strings now displays correctly

--- Models/CreateActivityViewModel.cs
+++ Models/CreateActivityViewModel.cs
@@ -4,20 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BeltExam.Models{
     public class CreateActivityViewModel{
-        [Required(ErrorMessage= "Title is required")]
         [MinLength(2, ErrorMessage="Title must have at least 2 characters")]
+        [Required(ErrorMessage= "Title is required")]
         public string title {get;set;}
-        [Required(ErrorMessage= "description is required")]
         [MinLength(10, ErrorMessage="Description must have at least 10 characters")]
+        [Required(ErrorMessage= "Description is required")]
         public string description {get;set;}
         [Required(ErrorMessage = "Address required")]
         public string address {get;set;}
         [Required(ErrorMessage= "Duration is required")]
-        public int duration {get;set;}
+        public int? duration {get;set;}
+        [Display(Name = "date")]
         [Required(ErrorMessage= "Date is required")]
-        public DateTime date {get;set;}
+        public DateTime? date {get;set;}
+        
         [Required(ErrorMessage= "Time is required")]
-        public DateTime time {get;set;}
+        public DateTime? time {get;set;}
         public string durationunit {get;set;}
     }
 }
